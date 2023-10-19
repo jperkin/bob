ca9406d095db585ca3ac57a991c02b962dfddd51
Jonathan Perkin <jonathan@perkin.org.uk>
2023-10-19 11:55:48 +0100

    Basic command line parsing and metadata.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Args::parse();
 }
